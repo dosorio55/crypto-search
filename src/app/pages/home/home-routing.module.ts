8bd113684f62e5a66f4d5e230dc74824adc6e9cf
dosorio55 <dosorio55@gmail.com>
2022-05-15 12:36:08 +0200

    home module generated with routing working

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadChildren: () => import('src/app/pages/home/home.module').then(m => m.HomeModule)
-  },
-  {
-    path: '**',
-    redirectTo: ''
+    component: HomeComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomeRoutingModule { }
