621e4913ceb2b20bebdd6cb58ffbfb4dc34b057a
dosorio55 <dosorio55@gmail.com>
2022-05-26 19:48:50 +0200

    working with user watch list coins

---
+++ src/app/pages/user-watch-list/components/user-coins/user-coins.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-coins',
+  templateUrl: './user-coins.component.html',
+  styleUrls: ['./user-coins.component.scss']
+})
+export class UserCoinsComponent implements OnInit {
+
+  // @Input() public 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
